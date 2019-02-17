5c94100c23d459ce8ba428a287c8bbde8ba46c41
Ariel Jacobs <ajacobs@ou.edu>
2019-02-17 01:28:50 -0800

    basic twillio script in python

---
+++ twillio/twilliotest.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+# Your Account SID from twilio.com/console
+account_sid = "ACbce739c8f483a6c8ed28343e6b2a1d37"
+# Your Auth Token from twilio.com/console
+auth_token  = "d034aa55e93ae2f34740bb916e24d943"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+19727624288", 
+    from_="+19729925293",
+    body="Your patient has requested your care! Please reach out to them as soon as you can.")
+
+print(message.sid)
