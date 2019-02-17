40c3ec80f7298a1517e99c89d1b89399e70e1ebd
Sam Jett <samjett247@gmail.com>
2019-02-17 08:50:03 -0800

    chart dev

--- frontend/src/chart_compiled.js
+++ frontend/src/chart_compiled.js
@@ -3,30 +3,138 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import React from 'react';
 
-const data = [
-      {name: 'date', Deep: 2000, Light: 2400, rem: 2400},
-      {name: 'Spring 2019', Deep: 3000, Light: 1398, rem: 2210},
-      {name: 'Summer 2019', Deep: 2000, Light: 9800, rem: 2290},
-      {name: 'Fall 2019', Deep: 2780, Light: 3908, rem: 2000},
-      {name: 'Spring 2020', Deep: 1890, Light: 4800, rem: 2181},
-      {name: 'Summer 2020', Deep: 2390, Light: 3800, rem: 2500},
-      {name: 'Fall 2020', Deep: 3490, Light: 4300, rem: 2100},
-];
+function parseParms(str) {
+  var pieces = str.split("&"), data = {}, i, parts;
+  // process each query pair
+  for (i = 0; i < pieces.length; i++) {
+    parts = pieces[i].split("=");
+    if (parts.length < 2) {
+      parts.push("");
+    }
+    data[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+  }
+  return data;
+}
+
+// Returns the token from the URL hash
+function getToken() {
+  //substring(1) to remove the '#'
+  hash = parseParms(document.location.hash.substring(1));
+
+  // Return hash for parsing later
+  return hash
+}
+
+// Send the user to the authorize endpoint for login and authorization
+function authorize(client_id) {
+  console.log('authorize fxn')
+  // window.location = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=" + client_id +"&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"; //&redirect_uri=" + settings.callback_url;
+  location.href = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=" + client_id +"&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+}
+
+// Make a call using our token to the Fitbit API
+function call_api(userId, token) {
+  var api_response;
+  $.ajax({
+    url:  "https://api.fitbit.com/1.2/user/" + userId + "/sleep/list.json?beforeDate=2019-02-18&sort=desc&offset=0&limit=1",// 'https://api.fitbit.com/1/user/'+ userId+ '/sleep/date/2019-02-16.json',
+    method: "GET",
+    async: false,
+    headers: {
+      "Authorization" : "Bearer " + token
+    },
+    success: function(response) {
+      // $("#response-code").html(JSON.stringify(response, null, 2));
+      // Prism.highlightAll();
+    api_response = JSON.stringify(response, null, 2);
+    }
+  });
+  return api_response
+  
+  }
+
+
+function get_user_data() {
+  // userId and token will change for each user, clientId will not change
+
+  // These are for testing
+  var userId = '7BRTPN'
+  var token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRKUzQiLCJzdWIiOiI3QlJUUE4iLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTUwOTQwNDMzLCJpYXQiOjE1NTAzNTc0OTh9.3LrFpkcWNeMq0KdhKf9WPyp6nQqCQ0wUka65uJNzomY"
+  var clientId = "22DJS4"
+
+  // authorize(clientId);
+  //FOR Production
+  api_response = call_api(userId, token); //FOR PRODUCTION
+  var response = JSON.parse(api_response)
+
+// Create a new response to consider a larger initial dataset
+  var new_response = JSON.parse(JSON.stringify(response));
+
+  dates = ['2019-02-16', '2019-02-15', '2019-02-14', '2019-02-13', '2019-02-12', '2019-02-11', '2019-02-10']
+  for (k = 0; k<120; k++){
+    res = response['sleep'][0]
+    if (k<7) {res['dateOfSleep'] = dates[k]}
+    new_response['sleep'].push(JSON.parse(JSON.stringify(res)))
+  }
+  var parsed_data = parse_data(new_response)
+  console.log(parsed_data)
+  // console.log(parsed_data)
+
+  //Save some example data
+  // download(JSON.stringify(parsed_data), 'example_data.json', 'text/plain');
+
+  // Check to see if the most recent data point is an anomaly
+  // ComputeProbabilities(parsed_data)
+  return parsed_data
+}
+
+function parse_data(full_data) {
+  
+  // This function will parse the json from the fitbit response
+  var return_data = []
+  data = full_data['sleep']
+
+  for (i = 0; i < data.length; i++) {
+    //Add the date into this entry
+    return_data[i] = {}
+    return_data[i]['date']= data[i]['dateOfSleep']
+
+    //define the summary dataset, and fill in the counts for each sleep type 
+    labels = ['deep', 'light', 'rem']
+
+    for (j=0;j<labels.length; j++) {
+      return_data[i][labels[j]] = data[i]['levels']['summary'][labels[j]]['minutes'] + 120*(0.5-Math.random()) //Change this if you dont want random data
+    }
+  // console.log(return_data)
+  }
+
+  return return_data
+
+}
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+
+const data = get_user_data();
 
 const TimeSeriesChart = props => (
   <div>
-    <LineChart width={800} height={400} data={props.data}
+    <AreaChart width={800} height={400} data={props.data}
     margin={{top: 5, right: 30, left: 20, bottom: 5}}>
         <XAxis dataKey="name" padding={{left: 30, right: 30}}/>
         <YAxis/>
         <CartesianGrid strokeDasharray="3 3"/>
         <Tooltip/>
         <Legend />
-        <Line type="monotone" dataKey="Deep" stroke="#8884d8" strokeWidth={3} activeDot={{r: 6}}/>
-        <Line type="monotone" dataKey="Light" stroke="#8884d8" strokeWidth={3} activeDot={{r: 6}}/>
-        <Line type="monotone" dataKey="rem" stroke="#8884d8" strokeWidth={3} activeDot={{r: 6}}/>
-
-  </LineChart>
+        <Area type="monotone" dataKey="Sam" stroke="#8884d8" fill="#8884d8" strokeWidth={3} activeDot={{r: 6}}/>
+        <Area type="monotone" dataKey="Janet" stroke="#82ca9d" fill='#82ca9d'/>
+        <Area  type="monotone" dataKey="Joe" stroke="#868788" fill='#868788'/>
+    </AreaChart>
   </div>
 );
 
--- test_for_anomaly.js
+++ test_for_anomaly.js
@@ -7,35 +7,102 @@ const epsilon = 0.01 // This is the confidence probability value to compare agai
 
 var JSONItems = [];
 function ComputeProbabilities(data){
-	console.log(data)
-	data_vec = math.Matrix()
-	labels = ['deep', 'light', 'rem']
+
+	var labels = ['deep', 'light', 'rem']
+	var data_mat = []
+	
 	// Generate vectors from json
-	for (i=0;i<data.length; i++) {
-	for (j=0; j<labels.len;j++) {
-		data_vec.get(i,j)= data[i]['sleepData'][labels[j]]
+	for (j=0; j<labels.length;j++){
+		data_mat[j]=[]
+	for (i=0;i<data.length; i++)  {
+		// console.log()
+		data_mat[j][i]=data[i]['sleepData'][labels[j]]
 	}
 	}
-	console.log(data_vec)
+	// mat = math.matrix(data_mat)
+	// console.log(mat._data)
+	smoothed_mat = Smooth(data_mat, m)
+	mean_vec = Mean(smoothed_mat)
+	covariance = Covariance(smoothed_mat, mean_vec)
 
 }
 
-function Smooth(vec, m) {
+function Smooth(mat, m) {
+	// Smooths the input matrix along one dimension
+	var len = mat[0].length
+	var smoothed_arr = []
 
+	for (j=0; j<3; j++) {
+		smoothed_arr[j]=[]
 
-	var vec = []
+		for (i=0; i<len;i++) {
+		if (i ==0 || i==len-1){
+		}
+		else {
+			smoothed_arr[j][i-1] = (1/3)*(mat[j][i] + mat[j][i-1] +mat[j][i+1])
+		}
+		}}
+	return smoothed_arr
 
-	return vec //Reduced length
-}
+	}
 
-function Mean(vec) {
+function Mean(mat) {
+	// Takes the mean of each feature
 	var mu=[]
+	for (j=0;j<mat.length; j++) {
+		arr = mat[j]
+		sum = arr.reduce(function(a, b) { return a + b; });
+    	avg = sum / arr.length;
+    	mu[j] = avg
+	}
+
 	return mu// nx1 dimensional vector
 }
 
-function Covariance(vec, m) {
-	var sigma=[]
-	return sigma // nxn dimensional matrix
+function cov_term(x, y) {
+	// computes the middle term of the covariance series
+	// Needed for Covariance fxn
+	var term= []
+	term[0] = [0,0,0]
+	term[1]= [0,0,0]
+	term[2] = [0,0,0]
+	console.log(x.length)
+	console.log(x[1]-y[1])
+
+	for (k=0;k<x.length; k++) {
+		for (j=0;j<y.length; j++) {
+			term[k][j] = (x[j]- y[j])*(x[k]-y[k])
+		}
+	}
+	console.log(term)
+	return term
+}
+
+function Covariance(mat, mu) {
+	// Computes the covariance of a matrix (nxn)
+
+	var len = mat[0].length
+	var sigma = []
+	console.log(mu)
+
+
+	sigma[0] = [0,0,0]
+	sigma[1]= [0,0,0]
+	sigma[2] = [0,0,0]
+
+	for (i=0;i<len; i++) {
+		//compute the covariance term for each data point 
+
+		covariance_term = cov_term([mat[0][i], mat[1][i], mat[2][i]], mu)
+		console.log(covariance_term)
+		var sigma= sigma.map(function (num, idx) {
+		  return num + array2[idx];
+		}); // [6,8,10,12]
+
+	}
+	console.log(sigma)
+
+	return sigma/len // nxn dimensional matrix
 }
 
 function Probability(vec, mu, sigma) {
