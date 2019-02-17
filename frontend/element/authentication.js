c5b934f375dc4b55d4feda3e614eddc97ac2d624
a2g1b <megan.chan@alumni.ubc.ca>
2019-02-16 19:33:35 -0800

    added frontend files

---
+++ frontend/element/authentication.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  var webAuth = new auth0.WebAuth({
+
+    domain:       'sleepapp.auth0.com',
+    clientID:     'FeAvjFlGnpHP4IbgcVo2UgfKkQUMzmK7'
+  });
+</script>
