40a3fbe9bb87497330f5e3495e9653b26fe238bd
a2g1b <megan.chan@alumni.ubc.ca>
2019-02-17 09:55:44 -0800

    commit

--- frontend/src/component/landing2.js
+++ frontend/src/component/landing2.js
@@ -3,16 +3,18 @@ import Statbox from './statbox.js'
 
 class Landing2 extends Component {
   render(){
-    var titles = ['Average # of hours slept','Average # of disruptions ','Average time to fall asleep']
+    var indices = [0,1,2]
+    var titles = ['Average REM sleep','Average deep sleep','Average light sleep']
+    var numbers = ['2.3 hours','3.1 hours','2.5 hours']
     return(
       <div className="background">
         <div className="container-fluid d-flex h-100 align-items-center">
-          {titles.map((value) => {
+          {indices.map((index) => {
             return(
               <div class="container col-4">
                 <div class="row">
                   <div class="col-md-100">
-                    <Statbox name = {value}/>
+                    <Statbox name = {titles[index]} number = {numbers[index]}/>
                     </div>
                   </div>
                 </div>
--- frontend/src/component/statbox.js
+++ frontend/src/component/statbox.js
@@ -4,6 +4,7 @@ class Statbox extends Component {
   render(){
     return (
       <div className="jumbotron">
+          <p class="text-align-center align-top"> <h5> {this.props.number} </h5> </p>
         <hr class="my-4"></hr>
         <p class="text-justify align-bottom"> <h5> {this.props.name} </h5> </p>
       </div>
